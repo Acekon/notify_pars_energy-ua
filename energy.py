c8072bc6fdaa592d6dc0bea26080abea7b170842
Acekon <acekon@gmail.com>
2024-07-11 08:39:10 +0300

    formated pars data

--- energy.py
+++ energy.py
@@ -38,15 +38,19 @@ console_handler.setFormatter(console_formatter)
 logger.addHandler(console_handler)
 
 
-def remove_empty_element_array(array: list) -> list:
-    return list(filter(None, array))
+def formated_array(array: list) -> list:
+    arr = list(filter(None, array))
+    result = []
+    i = 0
+    for _ in range(0, int(len(arr)/2)):
+        result.append(f'{arr[i]} {arr[i + 1]}')
+        i += 2
+    return result
 
 
 def telegram_send_text(chat_id: str, text: str):
     tg_url = f"https://api.telegram.org/bot{TELEGRAM_BOT}/sendMessage"
-    response = requests.post(
-        tg_url, json={"chat_id": chat_id, "parse_mode": "html", "text": text}
-    )
+    response = requests.post(tg_url, json={"chat_id": chat_id, "parse_mode": "html", "text": text})
     if response.json().get("ok"):
         logger.info(response.json())
     else:
@@ -66,28 +70,18 @@ def telegram_update_message(chat_id: str, message_id, text: str):
         },
     )
     if response.json().get("ok"):
-        logger.info(
-            {"chat_id": chat_id, "message_id": message_id, "tg": response.json()}
-        )
+        logger.info({"chat_id": chat_id, "message_id": message_id, "tg": response.json()})
     else:
-        logger.error(
-            {"chat_id": chat_id, "message_id": message_id, "tg": response.json()}
-        )
+        logger.error({"chat_id": chat_id, "message_id": message_id, "tg": response.json()})
 
 
 def telegram_delete_message(chat_id: str, message_id):
     tg_url = f"https://api.telegram.org/bot{TELEGRAM_BOT}/deleteMessage"
-    response = requests.post(
-        tg_url, json={"chat_id": chat_id, "message_id": message_id}
-    )
+    response = requests.post(tg_url, json={"chat_id": chat_id, "message_id": message_id})
     if response.json().get("ok"):
-        logger.info(
-            {"chat_id": chat_id, "message_id": message_id, "tg": response.json()}
-        )
+        logger.info({"chat_id": chat_id, "message_id": message_id, "tg": response.json()})
     else:
-        logger.error(
-            {"chat_id": chat_id, "message_id": message_id, "tg": response.json()}
-        )
+        logger.error({"chat_id": chat_id, "message_id": message_id, "tg": response.json()})
 
 
 def save_schedule_db(queue: int, text: str, day: str):
@@ -102,9 +96,7 @@ def save_schedule_db(queue: int, text: str, day: str):
 def save_message_id_db(message_id: int, queue: int):
     conn = sqlite3.connect("energy.db")
     c = conn.cursor()
-    sql_query = (
-        f'UPDATE energy SET message_id = "{message_id}" WHERE queue = "{queue}";'
-    )
+    sql_query = f'UPDATE energy SET message_id = "{message_id}" WHERE queue = "{queue}";'
     c.execute(sql_query)
     conn.commit()
     conn.close()
@@ -148,19 +140,15 @@ def get_schedules(queue: int):
         raise SystemExit()
     soup = BeautifulSoup(response.text, "html.parser")
     schedules = soup.find_all(class_="outage-periods-list")
-    formated_now_day = "\n".join(
-        remove_empty_element_array(schedules[0].text.strip().split("\n"))
-    )
-    formated_next_day = "\n".join(
-        remove_empty_element_array(schedules[1].text.strip().split("\n"))
-    )
+    formated_now_day = "\n".join(formated_array(schedules[0].text.strip().split("\n")))
+    formated_next_day = "\n".join(formated_array(schedules[1].text.strip().split("\n")))
     now_day_db = get_db(queue, "now_day")
     next_day_db = get_db(queue, "next_day")
     return (formated_now_day, now_day_db), (formated_next_day, next_day_db)
 
 
 def main():
-    day = [i for i in range(7, 20)]  # send only in number hours
+    day = [i for i in range(6, 20)]  # send only in number hours
     night = [i for i in range(21, 24)]  # send only in number hours
     logger.info("Start")
     for i in range(1, 7):  # count queue
@@ -170,16 +158,18 @@ def main():
         current_time = int(datetime.now().strftime("%#H"))
         message_id_db = get_message_id_db(queue=i)
         if site_now_day != db_now_day and current_time in day:
+            text = f'Черга {i}, Відключення на сьогодні:\n'
             save_schedule_db(queue=i, text=site_now_day, day="now_day")
-            telegram_send_text(text=site_now_day, chat_id=CHANNELS.get(i))
+            telegram_send_text(text=text + site_now_day, chat_id=CHANNELS.get(i))
             logger.info(f"Now day queue: {i} send")
         else:
             logger.info(f"Queue: {i} now not update")
         if site_now_day != db_now_day and current_time == day[0]:
             telegram_delete_message(chat_id=CHANNELS.get(i), message_id=message_id_db)
         if site_next_day != db_next_day and current_time in night and (site_next_day.find("ще очікуються") == -1):
+            text = f'Черга {i}, Відключення на завтра:\n'
             save_schedule_db(queue=i, text=site_next_day, day="next_day")
-            message_id = telegram_send_text(text=site_next_day, chat_id=CHANNELS.get(i))
+            message_id = telegram_send_text(text=text + site_next_day, chat_id=CHANNELS.get(i))
             save_message_id_db(message_id=message_id, queue=i)
             logger.info(f"Next day queue: {i} update")
         else:
